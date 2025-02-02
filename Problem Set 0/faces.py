63d0770934ea2b5651a66c5e91bf60ed5635ce7f
MrAnderson310 <sam.anderson3710@gmail.com>
2025-02-02 16:47:52 -0600

    Commenting faces.py

--- Problem Set 0/faces.py
+++ Problem Set 0/faces.py
@@ -1,12 +1,13 @@
-
+#Takes input from user and replaces all ":)" with "🙂" and all ":(" with "🙁" then prints it back
+#Replaces ":)" with "🙂" and ":(" with "🙁" then returns it 
 def convert(x):
     x = x.replace(":)", "🙂")
     x = x.replace(":(", "🙁")
     return x
-
+#Gets user input then calls covert(x) then prints the return of convert(x)
 def main():
     x = input()
     y = convert(x)
     print(y)
-
+#Calls main()
 main()
