abc73a693b7d206c54f668ab69e4d3a6d3c6aecc
MrAnderson310 <sam.anderson3710@gmail.com>
2025-02-02 15:01:08 -0600

    Indoor Voice Program
    Takes user input and converts it to lowercase

---
+++ indoor.py
@@ -0,0 +1,7 @@
+#This program will take in a string input from the user then translate it to lowercase
+#Gets input from user
+x = input()
+#Translates text to Lower Case
+y = x.lower()
+#Prints back out the users input but lower case
+print(y)
