014cb934dbae35ce974cc238d6029812de452596
MrAnderson310 <sam.anderson3710@gmail.com>
2025-02-02 15:17:34 -0600

    Created playback.py

---
+++ Problem Set 0/playback.py
@@ -0,0 +1,7 @@
+# Gets user input then for every space in the string replace it with "..."
+# Gets user input
+x = input()
+#Replaces spaces with "..."
+x = x.replace(" ", "...")
+#Print out the reworked string
+print(x)
