312274f5fb5194bf3e24b0de9dc3343ee1e9925a
MrAnderson310 <142929880+MrAnderson310@users.noreply.github.com>
2025-02-08 22:59:44 -0600

    Created grocery.py

---
+++ Problem Set 3/grocery.py
@@ -0,0 +1,36 @@
+def main():
+    groceryList = {}
+    while True:
+        item = getItem()
+        if item == "":
+            break
+        else:
+            groceryList = appendItem(item, groceryList)
+    
+    groceryList = sorted(groceryList)
+    print(groceryList)
+
+        
+
+            
+
+def appendItem(item, groceryList):
+    if item in groceryList:
+        groceryList[item] += 1
+    else:
+        groceryList[item] = 1
+    return groceryList
+    
+
+
+
+
+def getItem():
+    try:
+        item = input("Item: ")
+    except EOFError:
+        return ""
+    return item.capitalize()
+
+
+main()
